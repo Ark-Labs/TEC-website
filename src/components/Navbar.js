80edc93a8b475b89102d423ec1572e2d07e93e76
rabsztok <rabsztok@gmail.com>
2018-07-26 07:34:23 +0200

    Add mobile navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,14 @@ import styled from 'styled-components'
 import { Container } from 'bloomer'
 
 const Nav = styled.nav`
-  height: 128px;
+  min-height: 128px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   overflow: hidden;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+  }
 `
 
 const Logo = styled.img`
@@ -18,6 +21,38 @@ const Logo = styled.img`
 const Menu = styled.div`
   display: flex;
   font-size: 16px;
+  width: 100%;
+  @media (max-width: 768px) {
+    display: ${props => (props.open ? 'flex' : 'none')};
+    flex-direction: column;
+    align-items: center;
+}
+  }
+`
+
+const HamburgerLine = styled.div`
+  height: 3px;
+  width: 30px;
+  background-color: #e52839;
+  border-radius: 3px;
+`
+
+const Hamburger = styled(props => (
+  <a {...props}>
+    <HamburgerLine />
+    <HamburgerLine />
+    <HamburgerLine />
+  </a>
+))`
+  margin: 54px 20px;
+  cursor: pointer;
+  display: flex;
+  height: 21px;
+  flex-direction: column;
+  justify-content: space-between;
+  @media (min-width: 769px) {
+    display: none;
+  }
 `
 
 const NavbarLink = styled(Link)`
@@ -34,20 +69,34 @@ const NavbarLink = styled(Link)`
   }
 `
 
-const Navbar = ({ logo }) => (
-  <Container>
-    <Nav>
-      <Logo src={logo} />
-      <Menu>
-        <NavbarLink to="#about">About</NavbarLink>
-        <NavbarLink to="#marketplaces">Marketplaces / Ark Labs</NavbarLink>
-        <NavbarLink to="#blockchain">Blockchain</NavbarLink>
-        <NavbarLink to="#roadmap">Roadmap</NavbarLink>
-        <NavbarLink to="#team">Team</NavbarLink>
-        <NavbarLink to="#contact">Contact</NavbarLink>
-      </Menu>
-    </Nav>
-  </Container>
-)
+class Navbar extends React.Component {
+  constructor() {
+    super()
+    this.state = { open: false }
+  }
+
+  toggle = () => {
+    this.setState({ open: !this.state.open })
+  }
+
+  render() {
+    return (
+      <Container>
+        <Nav>
+          <Logo src={this.props.logo} />
+          <Hamburger onClick={this.toggle} />
+          <Menu open={this.state.open}>
+            <NavbarLink to="#about">About</NavbarLink>
+            <NavbarLink to="#marketplaces">Marketplaces / Ark Labs</NavbarLink>
+            <NavbarLink to="#blockchain">Blockchain</NavbarLink>
+            <NavbarLink to="#roadmap">Roadmap</NavbarLink>
+            <NavbarLink to="#team">Team</NavbarLink>
+            <NavbarLink to="#contact">Contact</NavbarLink>
+          </Menu>
+        </Nav>
+      </Container>
+    )
+  }
+}
 
 export default Navbar
